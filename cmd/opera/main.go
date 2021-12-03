113e838ecba74b418e4986f29663028da396af3d
kristofgazso <kristof.gazso@gmail.com>
2021-12-03 20:07:59 +0000

    increasing max procs

--- cmd/opera/main.go
+++ cmd/opera/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/Fantom-foundation/go-opera/cmd/opera/launcher"
 )
 
 func main() {
+	// increase max goroutines
+	oldMax := runtime.GOMAXPROCS(runtime.NumCPU() * 2)
+	fmt.Println("Old max procs:", oldMax, "new max procs:", runtime.NumCPU() * 2)
 	if err := launcher.Launch(os.Args); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
